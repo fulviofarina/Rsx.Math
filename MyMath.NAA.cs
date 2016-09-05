022c0e366d4c9b8c9d1cc6f747aa56cebf55567f
fulviofarina <fulviofarina@gmail.com>
2016-09-05 01:42:49 -0400

    added files

---
+++ MyMath.NAA.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Rsx.Math
+{
+    public partial class MyMath
+    {
+        /// <summary>
+        /// Saturation factor (lamda,t)
+        /// </summary>
+        public static Func<double, double, double> S = (l, t) => 1.0 - System.Math.Exp(-1.0 * l * t);
+
+        /// <summary>
+        /// Decay factor (lamda,t)
+        /// </summary>
+        public static Func<double, double, double> D = (l, t) => System.Math.Exp(-1.0 * l * t);
+
+        /// <summary>
+        /// Counting factor (lamda,t)
+        /// </summary>
+        public static Func<double, double, double> C = (l, t) => (1.0 - System.Math.Exp(-1.0 * l * t)) / (t * l);
+
+        /// <summary>
+        ///  (alpha)
+        /// </summary>
+        public static Func<double, double> Calpha = (alpha) =>
+        {
+            double intalpha = (2.0 * alpha) + 1.0;
+            double ecdalpha = System.Math.Pow(0.55, alpha);
+
+            return (0.429) / (ecdalpha * intalpha);
+        };
+
+        /// <summary>
+        /// (alpha,Qo,Er)
+        /// </summary>
+        public static Func<double, double, double, double> qoalpha = (alpha, Qo, Er) =>
+        {
+            return (Qo - 0.429) * System.Math.Pow(Er, -1.0 * alpha);
+        };
+
+        public static double NAvg = 6.02214179E+23;
+    }
+}
